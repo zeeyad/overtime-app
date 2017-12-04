5fea61dd9f087d58dca592d5738aff5f70fa1b67
Chloe Linux <zeeyadrahman@gmail.com>
2017-12-05 00:25:16 +0800

    use resource generator for Post

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+	
   describe "creation" do
   	before do
   		@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "john", last_name: "smith")
