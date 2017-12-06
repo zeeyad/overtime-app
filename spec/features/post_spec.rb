a946e029b31d1923908c20794b3250a38544515a
Chloe Linux <zeeyadrahman@gmail.com>
2017-12-06 15:19:05 +0800

    refactor index page to use Post partial

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do
 	before do
-		user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "john", last_name: "smith")
-  		login_as(user, :scope => :user)
+		@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "john", last_name: "smith")
+  		login_as(@user, :scope => :user)
 	end
  	
 
@@ -24,9 +24,9 @@ describe 'navigate' do
 	    end
 
 	    it 'has a list of posts' do
-	    	post1 = Post.create(date: Date.today, rationale: "Post1")
-	    	post2 = Post.create(date: Date.today, rationale: "Post2")
-
+	    	post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+	    	post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+	    	visit posts_path
 	    	expect(page).to have_content(/Post1|Post2/)
 	    end
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe Post, type: :model do
   describe "creation" do
 
   	before do
-  		@post = Post.create(date: Date.today, rationale: "Anything")
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "john", last_name: "smith")
+      login_as(@user, :scope => :user)
+  		@post = Post.create(date: Date.today, rationale: "Anything", user_id: @user.id)
   	end
 
   	it 'can be created' do
