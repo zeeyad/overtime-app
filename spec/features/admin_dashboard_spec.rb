b9a3e286c4fed0a98977f11d840cef76b1ed7a14
Chloe Linux <zeeyadrahman@gmail.com>
2017-12-07 02:50:45 +0800

    add admin dashboard permission

---
+++ spec/features/admin_dashboard_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'admin dashboard' do
+
+	it 'does not allow users to access without being signed in' do
+		visit admin_root_path
+		expect(current_path).to eq(new_user_session_path)
+	end
+
+	it 'can not be reached by a non admin users' do
+		user = FactoryBot.create(:user)
+		login_as(user, :scope => :user)
+		visit admin_root_path
+		expect(current_path).to eq(root_path)
+	end
+
+	it 'can be reached by an admin users' do
+		admin_user = FactoryBot.create(:admin_user)
+		login_as(admin_user, :scope => :user)
+		visit admin_root_path
+		expect(current_path).to eq(admin_root_path)
+	end
+end
