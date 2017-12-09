4a199b8b3eebae9972f334110ce20eb9636193ab
Chloe Linux <zeeyadrahman@gmail.com>
2017-12-09 21:26:10 +0800

    Implemented labels with status values on index for posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+	def status_btn status
+		status_span_generator status
+	end
+
+	private
+
+		def status_span_generator status
+			case status
+			when 'submitted'
+				content_tag(:span, status.titleize, class: 'btn btn-primary')
+			when 'approved'
+				content_tag(:span, status.titleize, class: 'btn btn-success')
+			when 'rejected'
+				content_tag(:span, status.titleize, class: 'btn btn-danger')
+			end
+		end
 end
--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -13,10 +13,8 @@ describe 'navigate' do
 		end
 
 		it 'has a status that can be edited on the form by an admin' do
-
 			choose 'post_status_approved'
 			click_on 'Save'
-
 			expect(@post.reload.status).to eq('approved')
 		end
 
@@ -26,6 +24,15 @@ describe 'navigate' do
 			login_as(@user, :scope => :user)
 			visit edit_post_path(@post)
 			expect(page).to_not have_content('Approved')
+ 		end
+
+ 		it 'should not be editable by the post creator if status is approved' do
+ 			logout(:user)
+			@user = FactoryBot.create(:user)
+			login_as(@user, :scope => :user)
+			@post.update(user_id: @user.id, status: 'approved')
+			visit edit_post_path(@post)
+			expect(current_path).to eq(root_path)
  		end
 	end
 end
