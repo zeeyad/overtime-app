35b3036a64471e719fc7f2586b28be5ebdb88a39
Chloe Linux <zeeyadrahman@gmail.com>
2017-12-12 03:17:41 +0800

    integrate basic manager notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,5 +28,6 @@ Rails.application.configure do
   config.assets.debug = true
   config.assets.quiet = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
