ebdbe5fe9da54d30dd81e4c519b8deadaee89fb6
Chloe Linux <zeeyadrahman@gmail.com>
2017-12-11 15:10:06 +0800

    brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ puts "1 User created"
 
 puts "1 Admin User created"
 100.times do |post|
-	Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id, overtime_request: 2.5)
+	Post.create(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a tincidunt justo, eu viverra turpis. Nulla in mattis lorem. Praesent vitae finibus libero. In volutpat nisi massa, eu varius metus pharetra eu. In libero justo, aliquam vel lectus eget, varius vulputate urna. Suspendisse vitae nibh fringilla, molestie sem in, sollicitudin purus. Integer vel vestibulum turpis. Ut ac lectus eu erat eleifend auctor. In in porttitor mi. Praesent quis magna quis felis venenatis tempor ut et felis. Suspendisse elementum commodo tortor at sagittis. Praesent sit amet dignissim lacus. Nam sed consectetur nibh, eu tincidunt turpis.", user_id: @user.id, overtime_request: 2.5)
 end
 
 puts "100 post has been created"
