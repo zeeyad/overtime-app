e0b2123e2567790f7e1675fd6158ce0cccb012ab
Chloe Linux <zeeyadrahman@gmail.com>
2017-12-12 02:25:18 +0800

    style employee hompeage pending audit approvals

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,8 +22,9 @@ end
 
 puts "100 post has been created"
 
-100.times do |audit_log|
-	AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-end
 
-puts "100 Audit Log has been created"
+AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+
+puts "3 Audit Log has been created"
