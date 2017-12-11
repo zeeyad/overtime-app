33df8808edd48436af2f786df32833199640cc15
Chloe Linux <zeeyadrahman@gmail.com>
2017-12-11 13:03:47 +0800

    blcok non admin users from accessing index page for audits

--- spec/features/audit_log_spec.rb
+++ spec/features/audit_log_spec.rb
@@ -20,8 +20,13 @@ describe 'AuditLog Feature' do
 			expect(page).to have_content(/JOHN/)
 		end
 
-		xit 'can not be accessed  by non admin users' do
+		it 'can not be accessed  by non admin users' do
+			logout(:user)
+			user = FactoryBot.create(:user)
+			login_as(user, :scope => :user)
 
+			visit audit_logs_path
+			expect(current_path).to eq(root_path)
 		end
 
 	end
