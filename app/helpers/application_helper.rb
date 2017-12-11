36eb2cd426898939cc344a9cdd0a5f4c9b120792
Chloe Linux <zeeyadrahman@gmail.com>
2017-12-11 14:02:34 +0800

    integrate application helper for status labels and integrated enums

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,25 @@ module ApplicationHelper
 	def active?(path)
 		"active" if current_page?(path)
 	end
+
+	def badge_btn status
+		badge_span_generator status
+	end
+
+	private
+
+		def badge_span_generator status
+			case status
+			when 'submitted'
+				content_tag(:span, status.titleize, class: 'badge badge-primary')
+			when 'approved'
+				content_tag(:span, status.titleize, class: 'badge badge-success')
+			when 'rejected'
+				content_tag(:span, status.titleize, class: 'badge badge-danger')
+			when 'pending'
+				content_tag(:span, status.titleize, class: 'badge badge-primary')
+			when 'confirmed'
+				content_tag(:span, status.titleize, class: 'badge badge-success')
+			end
+		end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,18 +1,3 @@
 module PostsHelper
-	def badge_btn status
-		badge_span_generator status
-	end
 
-	private
-
-		def badge_span_generator status
-			case status
-			when 'submitted'
-				content_tag(:span, status.titleize, class: 'badge badge-primary')
-			when 'approved'
-				content_tag(:span, status.titleize, class: 'badge badge-success')
-			when 'rejected'
-				content_tag(:span, status.titleize, class: 'badge badge-danger')
-			end
-		end
 end
